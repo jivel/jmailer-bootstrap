6eb30dc5a97c1d8c93cf18634cec9db9cc2a6405
josdem <joseluis.delacruz@gmail.com>
2015-10-08 19:59:04 -0500

    Adding aop

--- src/main/groovy/com/jos/dem/jmailer/advice/AfterThrowingAdvice.groovy
+++ src/main/groovy/com/jos/dem/jmailer/advice/AfterThrowingAdvice.groovy
@@ -5,21 +5,25 @@ import org.aspectj.lang.annotation.AfterThrowing
 import org.aspectj.lang.annotation.Aspect
 import org.springframework.stereotype.Component
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 @Aspect
 @Component
 class AfterThrowingAdvice {
 
-  Log log = LogFactory.getLog(AfterThrowingAdvice.class)
+  Logger log = LoggerFactory.getLogger(this.class)
 
-  @AfterThrowing(pointcut="execution(* com.jos.dem.controller.**.*(..))",throwing="customNameException")
+  @AfterThrowing(pointcut="execution(* com.jos.dem.jmailer.controller.**.*(..))",throwing="customNameException")
   def afterReturningMethod(JoinPoint joinPoint, RuntimeException customNameException) {
-    StringBuffer buffer = new StringBuffer("Ha ocurrido un error en " + joinPoint.getSignature().getName() + " ")
-    buffer.append("de " + joinPoint.getTarget().getClass().getName() + " - Argumentos:")
-    for(Object o:joinPoint.getArgs()){
-      buffer.append(o + " ")
+    def buffer = "Ha ocurrido un error en ${joinPoint.getSignature().getName()}"
+    buffer << "de ${joinPoint.getTarget().getClass().getName()}"
+    buffer << " - Argumentos:"
+    joinPoint.getArgs().each {
+      buffer << it
     }
-    buffer.append(" y el error " + customNameException.getMessage())
-    log.error(buffer.toString())
+    buffer << " y el error ${customNameException.getMessage()}"
+    log.error buffer.toString()
   }
 
 }
--- src/main/groovy/com/jos/dem/jmailer/web/EmailerController.groovy
+++ src/main/groovy/com/jos/dem/jmailer/web/EmailerController.groovy
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.http.ResponseEntity
 import org.springframework.http.HttpStatus
+import java.lang.RuntimeException
 import com.google.gson.Gson
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET
@@ -44,6 +45,7 @@ class EmailerController {
     log.debug "json: ${json}"
     MessageCommand command = new Gson().fromJson(json, MessageCommand.class);
     log.info "Sending contact email: ${command.dump()}"
+    throw new RuntimeException()
     return new ResponseEntity<String>("OK", HttpStatus.OK);
   }
 
