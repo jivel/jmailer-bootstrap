a12b43b042834c5ff79fb4a5dc6409bec7b6201c
josdem <joseluis.delacruz@gmail.com>
2015-10-12 20:57:54 -0500

    Deleting some lines

--- src/main/groovy/com/jos/dem/jmailer/controller/EmailerController.groovy
+++ src/main/groovy/com/jos/dem/jmailer/controller/EmailerController.groovy
@@ -4,16 +4,9 @@ import java.util.Map
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.servlet.ModelAndView
-import org.springframework.http.ResponseEntity
-import org.springframework.http.HttpStatus
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET
-import static org.springframework.web.bind.annotation.RequestMethod.POST
 
 import com.jos.dem.jmailer.service.EmailerService
 
