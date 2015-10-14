76cccda47f47d6b9ce41d67c2f88bd17ee193c87
josdem <joseluis.delacruz@gmail.com>
2015-10-14 10:35:39 -0500

    Deleting unused line

--- src/main/groovy/com/jos/dem/jmailer/advice/AfterThrowingAdvice.groovy
+++ src/main/groovy/com/jos/dem/jmailer/advice/AfterThrowingAdvice.groovy
@@ -1,6 +1,5 @@
 package com.jos.dem.jmailer.advice
 
-import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.AfterThrowing
 import org.aspectj.lang.annotation.Aspect
 import org.springframework.stereotype.Component
